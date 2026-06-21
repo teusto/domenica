f37b22cd8d82b2632b4c8b50312995349d6cc3bb
teusto <pteutoscano@gmail.com>
2026-06-21 22:59:11 +0100

    new hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,13 +1,26 @@
 import styles from "./styles/hero.module.scss";
 import DomenicaLogo from "../assets/imgs/logo.png"
+import HeroImage from "../assets/imgs/t2.png"
 import CallToAction from "../micro/CallToAction";
 import { useLocale } from "../utils/locales/LocaleProvider";
+import { Dither, Glitch, ImageTexture, ReflectivePlane, Shader } from "shaders/react";
 
 const Hero = () => {
     const { messages } = useLocale();
 
     return (
         <section className={styles.heroWrapper} id="init">
+            <div className={styles.heroBars}>
+                <Shader style={{ width: '100%', height: '130%'}}>
+                    <ReflectivePlane blur={1} edges="mirror">
+                    <Glitch colorBarIntensity={0.5} scanlineIntensity={0.3} intensity={0.4}>
+                    <Dither colorA="#F9F6EA" colorB="#E83B3A" spread={0.65} pixelSize={1} pattern="bayer2">
+                        <ImageTexture url={HeroImage} />
+                    </Dither>
+                    </Glitch>
+                    </ReflectivePlane>
+                </Shader>
+            </div>
             <div className={styles.heroHeader}>
                 <img src={DomenicaLogo} alt={messages.hero.logoAlt} />
             </div>
--- src/sections/Problem.tsx
+++ src/sections/Problem.tsx
@@ -6,13 +6,16 @@ const Problem = () => {
 
   return (
     <section className={styles.problemWrapper} id="problem">
-      <div className={styles.problemCardsContainer}>
-        {messages.problem.cards.map((card) => (
-          <div className={styles.problemCard} key={card.title}>
-            <p className={styles.cardTitle}>{card.title}</p>
-            <p className={styles.cardDescription}>{card.description}</p>
-          </div>
-        ))}
+      <div className={styles.problemCardsStack}>
+        <div className={styles.problemBehind}></div>
+        <div className={styles.problemCardsContainer}>
+          {messages.problem.cards.map((card) => (
+            <div className={styles.problemCard} key={card.title}>
+              <p className={styles.cardTitle}>{card.title}</p>
+              <p className={styles.cardDescription}>{card.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
       <div className={styles.problemText}>
         <h2>{messages.problem.summary}</h2>
